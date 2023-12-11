c0721bd9ee1469092e89ea8bbdf4b6674338e7d2
Ellet <hello@freshplatform.net>
2023-12-11 16:35:46 +0300

    Update GitHub repo

--- site/src/jsMain/kotlin/net/freshplatform/alrayada_landing_page/utils/constants/SectionsConstants.kt
+++ site/src/jsMain/kotlin/net/freshplatform/alrayada_landing_page/utils/constants/SectionsConstants.kt
@@ -8,7 +8,7 @@ object SectionsConstants {
     }
 
     object Footer {
-        const val REPOSITORY_URL = "https://github.com/freshtechtips/alrayada-landing-page"
+        const val REPOSITORY_URL = "https://github.com/freshplatform/alrayada-web"
     }
 
     object About {
